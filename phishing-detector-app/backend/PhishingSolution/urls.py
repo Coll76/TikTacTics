fc279839c406042933cc810ee2f17f25c44ef724
Coll76 <collins.kubu@gmail.com>
2024-10-04 04:04:40 +0530

    Add URLs for OAuth and email message endpoints

--- phishing-detector-app/backend/PhishingSolution/urls.py
+++ phishing-detector-app/backend/PhishingSolution/urls.py
@@ -1,22 +1,9 @@
 from django.urls import path
-from django.urls import path
-from .views import (
-    PhishingLinkCreateView,
-    PhishingLinkDetailView,
-    PhishingDataCreateView,
-    PhishingDataListView
-)
+
+from .views import OAuthAuthorizeView, OAuthRedirectView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
-    # URL pattern for creating a new phishing link
-    path('phishing-links/', PhishingLinkCreateView.as_view(), name='phishing-link-create'),
-
-    # URL pattern for getting and deleting a specific phishing link by ID
-    path('phishing-links/<int:pk>/', PhishingLinkDetailView.as_view(), name='phishing-link-detail'),
-
-    # URL pattern for creating new phishing data
-    path('phishing-data/', PhishingDataCreateView.as_view(), name='phishing-data-create'),
-
-    # URL pattern for listing all phishing data
-    path('phishing-data/list/', PhishingDataListView.as_view(), name='phishing-data-list'),
+    path('oauth-redirect/', OAuthRedirectView.as_view(), name='connect_email'),
+    path('oauth-authorize/', OAuthAuthorizeView.as_view(), name='oauth-authorize'), #new
 ]
