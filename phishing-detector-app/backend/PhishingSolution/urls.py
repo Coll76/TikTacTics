2235a4f7163d4442c487c18e7fff787150c8524d
Coll76 <collins.kubu@gmail.com>
2024-10-11 02:55:36 +0530

    Define url routes for the project. Added for auth and spam detection

--- phishing-detector-app/backend/PhishingSolution/urls.py
+++ phishing-detector-app/backend/PhishingSolution/urls.py
@@ -1,32 +1,22 @@
 from django.urls import path, include
-from .views import SpamDetectionAPIView, PhishingRegisterView, CustomLoginView, CustomLogoutView
-from .consumers import SpamEmailConsumer  # Import your WebSocket consumer
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from allauth.account.views import confirm_email
-
-
-# Define the WebSocket URL patterns here
-websocket_urlpatterns = [
-    path('ws/spam-alerts/', SpamEmailConsumer.as_asgi()),  # WebSocket URL for spam alerts
-]
-
-application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import SpamDetectionAPIView, PhishingRegisterView, CustomLoginView, CustomLogoutView, PasswordResetRequestView, PasswordResetView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('start-detection/', SpamDetectionAPIView.as_view(), name='start-detection'),
     path('auth/register/', PhishingRegisterView.as_view(), name='auth_register'),
     path('auth/login/', CustomLoginView.as_view(), name='auth_login'),
     path('auth/logout/', CustomLogoutView.as_view(), name='auth_logout'),
-    # Email confirmation URL
-    #path('auth/account-confirm-email/<str:key>/', CustomConfirmEmailView.as_view(), name='account_confirm_email'),
-    #path('verify-email/<uuid:token>/', VerifyEmailView.as_view(), name='verify_email'),
+    
+    # Include the dj_rest_auth URLs
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/', include('dj_rest_auth.registration.urls')),
+
+    #new
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
+    path('reset/<uidb64>/<token>/', PasswordResetView.as_view(), name='password-reset-confirm'),
 ]
+
